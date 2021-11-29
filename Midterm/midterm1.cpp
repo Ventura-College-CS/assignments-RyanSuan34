a4466be19e269663a1d1de8bf43e424c410dc2c6
Ryan Suan <69869173+RyanSuan34@users.noreply.github.com>
2021-11-29 22:29:49 +0000

    Midterm1

--- Midterm/midterm1.cpp
+++ Midterm/midterm1.cpp
@@ -3,35 +3,29 @@
 #include "midterm1.hpp"
 using namespace std;
 
-class Student
+int main()
 {
-  private:
-    int SID;
-    string SName;
-    double *scores;
-  public: 
-    Student()
-    {
-      SID=0;
-      SName="";
-      scores=nullptr;
-    }
-    Student(int i, string name, double *sc)
-    {
-      SID=i;
-      SName=name;
-      scores=sc;
-    }
-    int getID()
-    {
-      return SID;
-    }
-    string getName()
-    {
-      return SName;
-    }
-    double *getScores()
-    {
-      return scores;
-    }
-};
+  Student s1(100, "John", new double[3]{10, 20, 30});
+  Student s2(101, "Rane", new double[3]{10.2, 20.2, 30.2});
+  Student s3(102, "Tane", new double[3]{1, 2, 3});
+  Student s4(103, "Jane", new double[3]{20, 80, 90});
+  Student s5(104, "Jogn-rane", new double[3]{30, 40, 320});
+
+  Stack<Student> st;
+
+  st.push(s1);
+  st.push(s2);
+  st.push(s3);
+  st.push(s4);
+  st.push(s5);
+
+  cout << "Size: " << st.size() << endl;
+
+  while (!st.isEmpty())
+  {
+    Student res = st.pop();
+    cout << "SID: " << res.getID() << " , SName: " << res.getName() << " , scores: ";
+    cout << res.getScores()[0] << ", " << res.getScores()[1] << " , " << res.getScores()[2] << endl;
+  }
+  return 0;
+}
--- Midterm/midterm1.hpp
+++ Midterm/midterm1.hpp
@@ -2,6 +2,39 @@
 #include <vector>
 using namespace std;
 
+class Student
+{
+  private:
+    int SID;
+    string SName;
+    double *scores;
+  public: 
+    Student()
+    {
+      SID=0;
+      SName="";
+      scores=nullptr;
+    }
+    Student(int i, string name, double *sc)
+    {
+      SID=i;
+      SName=name;
+      scores=sc;
+    }
+    int getID()
+    {
+      return SID;
+    }
+    string getName()
+    {
+      return SName;
+    }
+    double *getScores()
+    {
+      return scores;
+    }
+};
+
 template<class T>
 class Stack
 {
