e46f9fe681a7a9b5775dbbf6e2a5566a4dc963ff
Ryan Suan <69869173+RyanSuan34@users.noreply.github.com>
2021-11-29 22:42:03 +0000

    StudentCompare

--- Midterm/midterm2.cpp
+++ Midterm/midterm2.cpp
@@ -33,4 +33,31 @@ class Student
     {
       return scores;
     }
+    double getSum()// Acquires the sum for the StudentCompare class
+    {
+      double sum = 0;
+      for(int i =0; i < numScores; i++)
+      {
+        sum += scores[i];
+      }
+      return sum;
+    }
+    void display()//Cleaner diplay of data
+    {
+      cout << "ID: " << getID() << " " << "Name: " << getName() << " " << "Scores: ";
+      for (int i = 0; i < numScores; i++)
+      {
+        cout << scores[i] << " ";
+      }
+      cout << "Score Sum: " << getSum(); 
+    }
 };
+
+class StudentCompare
+{
+  public:
+    bool operator()(Student s1, Student s2)
+    {
+      return s1.getSum()<s2.getSum();
+    }
+}
