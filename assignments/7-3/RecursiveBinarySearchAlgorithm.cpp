472aa306d2c00cb8f5dafec9f623ff87896e6da0
Ryan Suan <69869173+RyanSuan34@users.noreply.github.com>
2021-12-06 21:40:29 +0000

    7-3

---
+++ assignments/7-3/RecursiveBinarySearchAlgorithm.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int array[], int f, int l, int target)
+{
+  int m;
+
+  if (f>l)
+    return -1;
+  m = (f+l)/2;
+  if (array[m]==target)
+    return m;
+  else if (array[m]>target)
+    return binarySearch(array, f, m-1, target);
+  else 
+    return binarySearch(array, m+1, l, target);
+}
+
+int main()
+{
+  int array[] = {1,4,6,3,8,5,4,9};
+  int target = 5;
+
+  int n = sizeof(array)/sizeof(array[0]);
+
+  int f=0,l=n-1;
+  int index = binarySearch(array, f, l, target);
+
+  if (index != -1)
+    printf("Elemnt found at index %d", index);
+  else
+    printf("Elemnt not found in the array");
+  cout << endl; 
+  return 0;
+}
