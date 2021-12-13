66449ee2884bf79f40eb9cb413bb9e43f49b6621
Ryan Suan <69869173+RyanSuan34@users.noreply.github.com>
2021-12-13 08:30:40 +0000

    progress

--- Final/final1.cpp
+++ Final/final1.cpp
@@ -18,45 +18,58 @@ int binarySearch(Course array[], int target, int first, int last){
 }
 
 int main(){
-  Course array[10];
+  Course arr[10];
+  int select = 0;
 
-  array[0].setCourseID(0);
-  array[0].setCourseN("Linear Algebra");
-  array[0].setCourseC(5);
+  arr[0].setCourseID(0);
+  arr[0].setCourseN("Linear Algebra");
+  arr[0].setCourseC(5);
 
-  array[1].setCourseID(1);
-  array[1].setCourseN("Communications");
-  array[1].setCourseC(2);
+  arr[1].setCourseID(1);
+  arr[1].setCourseN("Communications");
+  arr[1].setCourseC(1);
 
-  array[2].setCourseID(2);
-  array[2].setCourseN("Physics: Elctr and Mag");
-  array[2].setCourseC(5);
+  arr[2].setCourseID(2);
+  arr[2].setCourseN("Physics: Elctr and Mag");
+  arr[2].setCourseC(5);
 
-  array[3].setCourseID(3);
-  array[3].setCourseN("Computer Science");
-  array[3].setCourseC(4);
+  arr[3].setCourseID(3);
+  arr[3].setCourseN("Computer Science");
+  arr[3].setCourseC(4);
 
-  array[4].setCourseID(4);
-  array[4].setCourseN("English");
-  array[4].setCourseC(3);
+  arr[4].setCourseID(4);
+  arr[4].setCourseN("English");
+  arr[4].setCourseC(3);
 
-  array[5].setCourseID(5);
-  array[5].setCourseN("Multi-var Calculus");
-  array[5].setCourseC(5);
+  arr[5].setCourseID(5);
+  arr[5].setCourseN("Multi-var Calculus");
+  arr[5].setCourseC(5);
 
-  array[6].setCourseID(6);
-  array[6].setCourseN("Differential Equations");
-  array[6].setCourseC(4);
+  arr[6].setCourseID(6);
+  arr[6].setCourseN("Differential Equations");
+  arr[6].setCourseC(4);
 
-  array[7].setCourseID(7);
-  array[7].setCourseN("Linear Algebra");
-  array[7].setCourseC(5);
+  arr[7].setCourseID(7);
+  arr[7].setCourseN("Physics: Optics and Heat");
+  arr[7].setCourseC(5);
 
-  array[8].setCourseID(8);
-  array[8].setCourseN("Linear Algebra");
-  array[8].setCourseC(5);
+  arr[8].setCourseID(8);
+  arr[8].setCourseN("ENGR: Elctronic circuits and devices");
+  arr[8].setCourseC(4);
 
-  array[9].setCourseID(9);
-  array[9].setCourseN("Linear Algebra");
-  array[9].setCourseC(5);
+  arr[9].setCourseID(9);
+  arr[9].setCourseN("Chemistry");
+  arr[9].setCourseC(1);
+
+  cout << "Enter Course ID (0-9): ";
+  cin >> select;
+
+  int n=sizeof(arr)/sizeof(arr[0]);
+  int first =0;
+  int last = n-1;
+  int index = binarySearch(arr, select, first, last);
+
+  if(index != -1){
+    
+  }
 }
