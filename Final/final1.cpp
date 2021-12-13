efc999bb71c4aeff087448b523ede22ea27898ba
Ryan Suan <69869173+RyanSuan34@users.noreply.github.com>
2021-12-13 07:55:31 +0000

    progress

---
+++ Final/final1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+class Course{
+  private:
+    int courseID;
+    string courseN;
+    int courseC;
+  public:
+    
+}
---
+++ Final/final1.hpp
@@ -0,0 +1,8 @@
+#ifndef FINAL1_HPP
+#define FINAL1_HPP
+#include <iostream>
+using namespace std;
+
+class Course{
+  
+}
