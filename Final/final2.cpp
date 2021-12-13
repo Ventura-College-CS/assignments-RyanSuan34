1247592623c140ff1d2db9bfa4d796460cf79e81
Ryan Suan <69869173+RyanSuan34@users.noreply.github.com>
2021-12-13 09:37:51 +0000

    progress

--- Final/final2.cpp
+++ Final/final2.cpp
@@ -7,7 +7,75 @@ using namespace std;
 void qsort(Course [], int, int);
 int partition(Course [], int, int);
 
-void qsort(Course array[], int first, int last){
-  int pivot;
-  if 
+void qsort(Course arr[], int first, int last){
+  int pivot_index;
+  if (first >= last)
+    return; 
+  pivot_index = partition(arr, first, last);
+  qsort(arr, first, pivot_index-1);
+  qsort(arr, pivot_index+1, last);
+}
+
+int partition(Course arr[], int first, int last){
+  int pivot = arr[last];
+  int i=-1;
+  for (int j=0; j<last; j++){
+    if(arr[j] < pivot)
+      swap(arr[i++], num[j]);
+  }
+  swap(arr[i+1],arr[last]);
+  return i+1;
+}
+
+int main(){
+  Course arr[10];
+  int select = 0;
+
+  arr[0].setCourseID(0);
+  arr[0].setCourseN("Linear Algebra");
+  arr[0].setCourseC(5);
+
+  arr[1].setCourseID(1);
+  arr[1].setCourseN("Communications");
+  arr[1].setCourseC(1);
+
+  arr[2].setCourseID(2);
+  arr[2].setCourseN("Physics: Elctr and Mag");
+  arr[2].setCourseC(5);
+
+  arr[3].setCourseID(3);
+  arr[3].setCourseN("Computer Science");
+  arr[3].setCourseC(4);
+
+  arr[4].setCourseID(4);
+  arr[4].setCourseN("English");
+  arr[4].setCourseC(3);
+
+  arr[5].setCourseID(5);
+  arr[5].setCourseN("Multi-var Calculus");
+  arr[5].setCourseC(5);
+
+  arr[6].setCourseID(6);
+  arr[6].setCourseN("Differential Equations");
+  arr[6].setCourseC(4);
+
+  arr[7].setCourseID(7);
+  arr[7].setCourseN("Physics: Optics and Heat");
+  arr[7].setCourseC(5);
+
+  arr[8].setCourseID(8);
+  arr[8].setCourseN("ENGR: Elctronic circuits and devices");
+  arr[8].setCourseC(4);
+
+  arr[9].setCourseID(9);
+  arr[9].setCourseN("Chemistry");
+  arr[9].setCourseC(1);
+
+  cout << "Enter Course ID (0-9): ";
+  cin >> select;
+
+  int n=sizeof(arr)/sizeof(arr[0]);
+  qsort(arr, 0, n-1)
+  for(int i=0; i<n;i++)
+    cout arr[i]<<" "
 }
